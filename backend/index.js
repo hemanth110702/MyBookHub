19b1e73dcd68982c8a1486e88068bcf18007ce14
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-14 08:10:32 +0530

    added routes and pages in frontend

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to MyBookHub");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import CreateBook from "../pages/CreateBook";
+import DeleteBook from "../pages/DeleteBook";
+import EditBook from "../pages/EditBook";
+import ShowBook from "../pages/ShowBook";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books">
+          <Route path="create" element={<CreateBook />} />
+          <Route path="details/:id" element={<ShowBook />} />
+          <Route path="edit/:id" element={<EditBook />} />
+          <Route path="delete/:id" element={<DeleteBook />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
