f11e87b1eea251eb17f1a1de48050beb75bf97ea
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-14 10:44:53 +0530

    updated import paths and cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDbURL } from "./config.js";
+import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
@@ -16,10 +18,22 @@ mongoose
   });
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-type"],
+  })
+);
 
-app.get("/", (req, res) => {
-  return res.status(200).send("Welcome to MyBookHub");
+app.get("/", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.send(books);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("An error while fetching books");
+  }
 });
 
 app.use("/books", booksRoute);
