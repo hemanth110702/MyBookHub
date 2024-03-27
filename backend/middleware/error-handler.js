45c9a474c8daf743f15a5f384738e43b4e08dc47
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-03-28 04:55:34 +0530

    restart

--- backend/middleware/error-handler.js
+++
@@ -1,5 +0,0 @@
-// error-handler.js
-export function errorHandler(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json({ success: false, message: "Internal Server Error" });
-}
